9041c2fd5aacb56f205350be86837fcb27f93eb4
joerg <joerg@debian>
2018-11-10 23:49:41 +0100

    tiny changes to ranmedi generator

--- ranmedi/ranmedi.py
+++ ranmedi/ranmedi.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 def main():
    lx=512
    lz=512
-   xi_x=15.0
-   xi_z=15.0
+   xi=15.0
    eps=20.0
    kappa=0.5
    fct=2 #1 Gaussian, 2 exponential
@@ -14,10 +13,10 @@ def main():
    Rand=np.zeros((lx,lz),dtype=np.complex128)
    np.random.seed(43)
    if fct==1:
-      rand=gauss(lx,lz,xi_x,xi_z,eps)
+      rand=gauss(lx,lz,xi,xi_z,eps)
       label="Gaussian random field"
    if fct==2:
-      rand=exponential(lx,lz,xi_x,xi_z,eps)
+      rand=exponential(lx,lz,xi,xi_z,eps)
       label="Exponential random field"
 
    im=plt.imshow(rand, cmap=plt.cm.jet)
@@ -26,12 +25,12 @@ def main():
    plt.show()
 
 
-def gauss(lx,lz,xi_x,xi_z,eps):
+def gauss(lx,lz,xi,eps):
    psdf=np.zeros((lx,lz),dtype=np.complex128)
    for i in range(0,lx/2):
       for j in range(0,lz/2):
          kr2=(i*2*np.pi/lx)**2+(j*2*np.pi/lz)**2
-         psdfi=eps**2*np.pi*(xi_x)**2*np.exp(-kr2*(xi_x)**2/4)
+         psdfi=eps**2*np.pi*(xi)**2*np.exp(-kr2*(xi)**2/4)
          random=2*np.pi*(np.random.rand()-0.5)
          psdf[i,j]=np.sqrt(psdfi)*np.exp(1.j*random)
          psdf[i,lz-j-1]=-np.conjugate(np.sqrt(psdfi)*np.exp(1.j*random))
@@ -39,12 +38,12 @@ def gauss(lx,lz,xi_x,xi_z,eps):
    rand=np.real((np.fft.ifft2(psdf)))
    return rand
 
-def exponential(lx,lz,xi_x,xi_z,eps):
+def exponential(lx,lz,xi,eps):
    psdf=np.zeros((lx,lz),dtype=np.complex128)
    for i in range(0,lx/2):
       for j in range(0,lz/2):
          kr2=(i*2*np.pi/lx)**2+(j*2*np.pi/lz)**2
-         psdfi=eps**2*np.pi*(xi_x)**2/(1.+kr2*xi_x**2)**1.5
+         psdfi=eps**2*np.pi*(xi)**2/(1.+kr2*xi**2)**1.5
          random=2*np.pi*(np.random.rand()-0.5)
          psdf[i,j]=np.sqrt(psdfi)*np.exp(1.j*random)
          psdf[i,lz-j-1]=-np.conjugate(np.sqrt(psdfi)*np.exp(1.j*random))
