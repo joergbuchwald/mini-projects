7dcff108d394b546555139573fa42f54f4484a0f
Dmitri Naumov <dmitri.naumov@ufz.de>
2020-10-05 19:38:48 +0200

    Extend xslt to moving solid properties into medium

--- tools/shiftparam2medium.py
+++ tools/shiftparam2medium.py
@@ -9,7 +9,7 @@ def transform(tree, xslt):
     return transform(tree)
 
 
-def xslt():
+def xsltMoveSolidPropertyToMedium(property_name):
     return ET.XML('''\
     <xsl:stylesheet version="1.0"
         xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
@@ -20,7 +20,30 @@ def xslt():
                 <xsl:apply-templates select="node()|@*"/>
             </xsl:copy>
         </xsl:template>
-    </xsl:stylesheet>''')
+
+        <!-- Remove this property. -->
+        <xsl:template match="//phase[./type='Solid']/properties/property[./name='PROPERTY_NAME']"/>
+
+        <!-- Copy it into /medium/properties if the latter exists. -->
+        <xsl:template match="//medium/properties">
+            <xsl:copy>
+                <xsl:apply-templates select="@*" />
+                <xsl:copy-of select="//phase[./type='Solid']/properties/property[./name='PROPERTY_NAME']"/>
+                <xsl:apply-templates select="node()" />
+            </xsl:copy>
+        </xsl:template>
+
+        <!-- Copy it into /medium/properties if the latter does _not_ exists. -->
+        <xsl:template match="//medium[not(properties)]">
+            <xsl:copy>
+                <xsl:apply-templates select="@*" />
+                <properties>
+                    <xsl:copy-of select="//phase[./type='Solid']/properties/property[./name='PROPERTY_NAME']"/>
+                </properties>
+                <xsl:apply-templates select="node()" />
+            </xsl:copy>
+        </xsl:template>
+    </xsl:stylesheet>'''.replace('PROPERTY_NAME', property_name))
 
 
 class moveparametersXML(object):
@@ -98,7 +121,12 @@ if __name__ == "__main__":
 
     tree = ET.parse(sys.argv[1])
     encoding = tree.docinfo.encoding
-    tree = transform(tree, xslt())
+
+    for property in [
+            'permeability', 'porosity', 'storage', 'biot_coefficient'
+    ]:
+        tree = transform(tree, xsltMoveSolidPropertyToMedium(property))
+
     ET.indent(tree, space="    ")
     tree.write(sys.argv[2],
                encoding=encoding,
