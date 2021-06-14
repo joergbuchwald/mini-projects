0f0a87c7bc336f3c7c3cfa6d7c7d31d227bf606b
joergbuchwald <joerg.buchwald@ufz.de>
2021-06-14 17:13:04 +0200

    use new ogs version

--- tools/addEffectiveThermalConductivityPorosityMixing.py
+++ tools/addEffectiveThermalConductivityPorosityMixing.py
@@ -4,8 +4,8 @@ from ogs6py.ogs import OGS
 
 def main(filename):
     f = OGS(INPUT_FILE=filename, PROJECT_FILE=filename)
-    f.addBlock("property", parent_xpath="./media/medium/properties", taglist=["name","type"], textlist=["thermal_conductivity", "EffectiveThermalConductivityPorosityMixing"])
-    f.writeInput()
+    f.add_block("property", parent_xpath="./media/medium/properties", taglist=["name","type"], textlist=["thermal_conductivity", "EffectiveThermalConductivityPorosityMixing"])
+    f.write_input()
 
 if __name__ == '__main__':
     main(sys.argv[1])
