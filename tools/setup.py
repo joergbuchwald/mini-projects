d9155c0d45eb0d59473c4480412170bb39e74c64
joergbuchwald <joerg.buchwald@ufz.de>
2021-12-20 19:58:04 +0100

    add setup.py for vanGenuchten model

---
+++ tools/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(name="vanGenuchten",
+      version=0.1,
+      maintainer="Jörg Buchwald",
+      maintainer_email="joerg_buchwald@ufz.de",
+      author="Jörg Buchwald",
+      author_email="joerg.buchwald@ufz.de",
+      install_requires=[],
+      py_modules=["vanGenuchten"],
+      packages=[])
